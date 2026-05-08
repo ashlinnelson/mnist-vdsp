de709679a9675476cbeb5d5aed91c2529e0a5570
Ashlin <ashlindev4@gmail.com>
2026-05-08 13:05:46 +0530

    added evaluation

--- code/network_garg.py
+++ code/network_garg.py
@@ -1,8 +1,8 @@
 from eqn_garg import *
 from brian2 import *
 
-array = np.zeros((1, n_input))
-pixel_input = TimedArray(array, dt=50*ms) # dt will be overwritten in notebook
+temp = np.zeros((1, n_input))
+pixel_input = TimedArray(temp, dt=50*ms) # dt will be overwritten in notebook
 t_offset = 0 * ms
 
 def build_network_train():
@@ -56,5 +56,47 @@ def build_network_train():
 
     net = Network(inp_group, exc_group, inh_group, S_inp_exc, S_exc_inh, S_inh_exc, 
                   spike_monitor, normalize_weights)
+    
+    return net, inp_group, spike_monitor
+
+
+def build_network_test():
+    # Input Layer
+    inp_group = NeuronGroup(n_input, eqs_in, threshold='v > V_thresh', 
+							reset='v = V_reset', refractory=refrac_in, 
+							method='euler', name='inp')
+    inp_group.v = V_rest
+    inp_group.I = 0 * volt
+    
+    # Excitatory Layer
+    exc_group = NeuronGroup(n_e, eqs_e_test, threshold='v > adaptive_thresh', reset='v = E_reset', 
+							refractory=E_refrac, method='euler', name='exc')
+    exc_group.v = E_rest
+    
+    # Inhibitory Layer
+    inh_group = NeuronGroup(n_i, eqs_i, threshold='v > I_thresh', 
+                            reset='v = I_reset', refractory=I_refrac, 
+                            method='euler', name='inh')
+    inh_group.v = I_rest
+    
+    # Input -> Excitatory (No VDSP)
+    S_inp_exc = Synapses(inp_group, exc_group, model=eqs_vdsp,
+                         on_pre='ge_post += w',name='s_inp_exc')
+    S_inp_exc.connect(p=1.0)
+    S_inp_exc.delay = 'rand() * max_delay'
+
+    # Excitatory -> Inhibitory (1-to-1)
+    S_exc_inh = Synapses(exc_group, inh_group, on_pre='ge_post += w_ei', name='s_exc_inh')
+    S_exc_inh.connect(j='i')
+
+    # Inhibitory -> Excitatory (Winner-Take-All lateral inhibition)
+    S_inh_exc = Synapses(inh_group, exc_group, on_pre='gi_post += w_ie', name='s_inh_exc')
+    S_inh_exc.connect(condition='i != j')
+
+    # Monitors
+    spike_monitor = SpikeMonitor(exc_group, name='sp_exc')
+    
+    net = Network(inp_group, exc_group, inh_group, S_inp_exc, S_exc_inh, S_inh_exc, 
+                  spike_monitor)
     
     return net, inp_group, spike_monitor
--- code/utils.py
+++ code/utils.py
@@ -16,4 +16,33 @@ def plot_weights(weights, n_e):
         ax.axis('off')
     plt.subplots_adjust(wspace=0.1, hspace=0.1)
     plt.show()
+
+def plot_confusion_matrix(cm):
+    fig, ax = plt.subplots(figsize=(10, 8))
+    cax = ax.imshow(cm, interpolation='nearest', cmap='Blues')
+    fig.colorbar(cax)
+
+    # Set up the axes labels
+    classes = np.arange(10)
+    ax.set_xticks(classes)
+    ax.set_yticks(classes)
+    ax.set_xticklabels(classes)
+    ax.set_yticklabels(classes)
     
+    # Add axis titles
+    ax.set_ylabel('True Digit (Actual)', fontsize=14)
+    ax.set_xlabel('Predicted Digit (SNN Output)', fontsize=14)
+    ax.set_title('SNN Confusion Matrix', fontsize=16, pad=20)
+
+    # Loop over the matrix data and add the raw numbers as text inside the boxes
+    # change the text color to white if the box background is very dark
+    threshold = cm.max() / 2.0
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(j, i, format(cm[i, j], 'd'),
+                    ha="center", va="center",
+                    color="white" if cm[i, j] > threshold else "black",
+                    fontsize=12)
+
+    plt.tight_layout()
+    plt.show()
